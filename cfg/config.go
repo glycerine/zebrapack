b7fd9405f784a6eb8f0be632296c0995204299bc
Jason E. Aten <j.e.aten@gmail.com>
2016-12-26 12:29:07 -0600

    doc++

--- cfg/config.go
+++ cfg/config.go
@@ -30,7 +30,7 @@ func (c *ZebraConfig) DefineFlags(fs *flag.FlagSet) {
 
 	fs.StringVar(&c.WriteSchema, "write-schema", "", "write schema header to this file; - for stdout")
 	fs.BoolVar(&c.GenSchemaId, "genid", false, "generate a fresh random zebraSchemaId64 value for your schema")
-	fs.BoolVar(&c.UseZid, "fast", false, "for speed and type safety, instead of writing field names in structs, write the numeric zid into the msgpack. See also -write-schema to generate an external schema description to read/write in other languages.")
+	fs.BoolVar(&c.UseZid, "fast", false, "generate zebrapack serializers instead of msgpack2. For speed and type safety, instead of writing field names -- to identify fields -- zebrapack writes their zid number in the serialization. See also -write-schema to generate an external schema description to read/write in other languages.")
 	fs.BoolVar(&c.ReadStringsFast, "fast-strings", false, "for speed when reading a string in a message that won't be reused, this flag means we'll use unsafe to cast the string header and avoid allocation.")
 	fs.StringVar(&c.SchemaToGo, "schema-to-go", "", "path to schema that will be converted to Go and written on stdout")
 }
