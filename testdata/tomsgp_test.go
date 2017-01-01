a388e55b75424498c74a717a02e643e8e0f0eeb6
Jason E. Aten <j.e.aten@gmail.com>
2016-12-31 22:22:47 -0500

    atg. tests confirm the Encode and MarshalMsg producing same output

--- testdata/tomsgp_test.go
+++ testdata/tomsgp_test.go
@@ -26,6 +26,12 @@ func Test060ConvertZebraPackToMsgpack2(t *testing.T) {
 		data, err := e.MarshalMsg(nil)
 		panicOn(err)
 
+		// confirm that encode works the same
+		var encbuf bytes.Buffer
+		err = msgp.Encode(&encbuf, &e)
+		panicOn(err)
+		cv.So(string(encbuf.Bytes()), cv.ShouldEqual, string(data))
+
 		// turn it into msgpack2 and display as json
 
 		m2, _, err := zSchema.ZebraToMsgp2(data)
@@ -50,6 +56,12 @@ func Test060ConvertZebraPackToMsgpack2(t *testing.T) {
 		_, err = msgp.CopyToJSON(&json, bytes.NewBuffer(m2))
 		panicOn(err)
 		cv.So(string(json.Bytes()), cv.ShouldEqual, `{"beta":"hello","P":43,"arr":[0,0,0,0,0,0],"MyTree":{"Chld":[{"Chld":[{"Str":"leaf"}],"Str":"mid"}],"Str":"root"}}`)
+
+		// confirm that Encode works the same as MarshalMsg
+		var encbuf2 bytes.Buffer
+		err = msgp.Encode(&encbuf2, &e)
+		panicOn(err)
+		cv.So(string(encbuf2.Bytes()), cv.ShouldEqual, string(data))
 
 	})
 }
