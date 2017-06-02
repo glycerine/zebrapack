26ed5b77df4554b0b4eb01eced5dfd5091ddfcc3
Jason E. Aten <j.e.aten@gmail.com>
2017-06-02 13:55:20 -0500

    ft. new map_test.go for adding zid to maps

---
+++ cmd/addzid/map_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"testing"
+
+	cv "github.com/glycerine/goconvey/convey"
+)
+
+func Test910MapAddZid(t *testing.T) {
+
+	cv.Convey("Given a struct with a map[string]bool, addzid should add a zid to the map", t, func() {
+		cv.Convey("then addzid should add a zid for both the time.Time and the int field.", func() {
+			in1 := "type HasMap struct {Jobid map[string]bool }"
+			ex1 := "package main type HasMap struct { Jobid map[string]bool `zid:\"0\"`}"
+			added := ExtractStringAddZid(in1)
+			p("added='%v'", added)
+			cv.So(added, ShouldStartWithModuloWhiteSpace, ex1)
+		})
+	})
+}
